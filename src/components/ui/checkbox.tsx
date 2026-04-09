528f1a62d3aadd696386df52b8bfd7a4d794d496
Stephan Burn <stephan.burn@gmail.com>
2026-04-09 08:05:28 +0100

    Fix lint errors in character sheet build

--- src/components/ui/checkbox.tsx
+++ src/components/ui/checkbox.tsx
@@ -4,7 +4,9 @@ import * as React from 'react'
 import { cn } from '@/lib/utils'
 
 export interface CheckboxProps
-  extends Omit<React.InputHTMLAttributes<HTMLInputElement>, 'type'> {}
+  extends Omit<React.InputHTMLAttributes<HTMLInputElement>, 'type'> {
+  checked?: boolean
+}
 
 export const Checkbox = React.forwardRef<HTMLInputElement, CheckboxProps>(
   ({ className, ...props }, ref) => (
